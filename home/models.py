ebab9148852e2feae3a103fbb4d5b88e3dc129d7
guan <kevinx6000@gmail.com>
2017-02-01 18:00:11 +0800

    Create first model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ModelTest(models.Model):
+    name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15)
+    note = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.name
