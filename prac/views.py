48f0b445915fcd87458c46ca2bd9047fde5a8f71
guan <kevinx6000@gmail.com>
2017-02-01 16:40:50 +0800

    First template

--- prac/views.py
+++ prac/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(req):
-    return HttpResponse('安安')
+    return render_to_response('home.html', {'hello':'Hello', 'world':'world!'})
